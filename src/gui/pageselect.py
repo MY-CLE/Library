992d29b16cb64ddeeb2b5f914ebb2e522e248e4b
mabuit03 <mabuit03@hs-esslingen.de>
2022-12-05 09:57:15 +0100

    change communication between widgets

--- src/gui/pageselect.py
+++ src/gui/pageselect.py
@@ -1,17 +1,19 @@
 from gui.windows.loginWindow import LoginWindow
 from gui.windows.landingWindow import LandingWindow
 from PyQt6.QtWidgets import (QMainWindow,QStackedWidget)
+from PyQt6.QtCore import pyqtSignal
 
-class PageSelect(QMainWindow):
+class PageSelect(QMainWindow):    
     def __init__(self):
         super(QMainWindow, self).__init__()
+        self.userId = None
         #initiation of Widows
         self.setWindowTitle('Liberary')
         self.loginWindow = LoginWindow(self)
-        self.landingWindow = LandingWindow()
+        self.landingWindow = LandingWindow(self)
         
         #Add them to ustom Stacked Widget
-        self.widget = Pages(self.loginWindow)
+        self.widget = Pages()
         self.widget.addWidget(self.loginWindow)
         self.widget.addWidget(self.landingWindow)
         
@@ -19,15 +21,21 @@ class PageSelect(QMainWindow):
         #Display the Loginpage as default
         self.widget.setCurrentIndex(0)
         self.widget.show()
+        self.loginWindow.sendUser.connect(self.setUser)
+    
+    def setUser(self, user):
+        self.userId = user
+        print('main window got signal')
+        self.changeStackedWidget(1)
+        self.landingWindow.setUserid(self.userId)
         
-#This Custom StackedWidegt is basicly the same as a normal one.
+    def changeStackedWidget(self, index):
+        self.widget.setCurrentIndex(index)
+        
+        
+#This Custom Stac0kedWidegt is basicly the same as a normal one.
 #but it has a custom function which allows the Children of it to
 #change the visble widget
-class Pages(QStackedWidget):
-    def __init__(self, LoginWindow):
+class Pages(QStackedWidget):    
+    def __init__(self):
         super(QStackedWidget, self).__init__()
-        self.login = LoginWindow
-         
-    def changeStackedWidget(self):
-        index = self.login.isLogedIn()
-        self.setCurrentIndex(index)
