7feb50d7d9da3c8bf9a0ae4789fa0ce26e9e01e8
emir1312 <tealaemir@gmail.com>
2022-12-07 21:49:48 +0100

    load detailsPage on book(img) click

---
+++ src/GUI/Windows/detailsWindow.py
@@ -0,0 +1,28 @@
+from GUI.elements.detailsView import DetailsView
+from GUI.elements.header import CustHeader
+from PyQt6.QtWidgets import (QHBoxLayout, QVBoxLayout, QFrame)
+from PyQt6.QtCore import QSize
+
+
+class DetailWindow(QFrame):
+    def __init__(self, parent=None):
+        super(QFrame, self).__init__()
+        self.setWindowTitle("Details")
+        self.setMinimumSize(QSize(1080, 720))
+        self.setObjectName("detailsWindow")
+        # self.setFrameShape(QFrame.Shape.NoFrame)
+        # self.setLineWidth(0)
+
+        self.header = CustHeader()
+        self.details = DetailsView()
+        viewQVlayout = QVBoxLayout()
+        viewQVlayout.addWidget(self.header)
+        viewQVlayout.addWidget(self.details)
+        viewQVlayout.addStretch()
+        viewQVlayout.setContentsMargins(0, 0, 0, 0)
+
+        mainQHlayout = QHBoxLayout()
+        mainQHlayout.addLayout(viewQVlayout)
+        mainQHlayout.setContentsMargins(0, 0, 0, 0)
+
+        self.setLayout(mainQHlayout)
--- src/gui/helper/downloadImg.py
+++ src/gui/helper/downloadImg.py
@@ -1,67 +1,92 @@
 import os
 import requests
 from PyQt6.QtCore import pyqtSignal, QRunnable, QObject, QThreadPool
-from PyQt6.QtWidgets import (QVBoxLayout, QWidget,QLabel)
+from PyQt6.QtWidgets import (QVBoxLayout, QWidget, QLabel, QMainWindow)
 from PyQt6.QtGui import QPixmap, QImage
+from GUI.Windows.detailsWindow import DetailWindow
+
+
 class DownloadImg(QRunnable):
-    def __init__(self, dic) :
+    def __init__(self, dic):
         super().__init__()
         self.dic = dic
         self.signals = DownloadImgSignals()
-    
+
     def run(self):
-        self.dic['localpath'] = os.path.join(os.path.abspath('src/assets/covers/'), f"Image_{str(self.dic['id'])}.jpg")
+        self.dic['localpath'] = os.path.join(os.path.abspath(
+            'src/assets/covers/'), f"Image_{str(self.dic['id'])}.jpg")
         if not os.path.exists(self.dic['localpath']):
             img_data = requests.get(self.dic['img-src']).content
             with open(self.dic['localpath'], 'wb') as handler:
                 handler.write(img_data)
         self.signals.returnBook.emit(self.dic)
         self.signals.finished.emit()
-        
+
+
 class DownloadImgSignals(QObject):
     returnBook = pyqtSignal(dict)
-    finished = pyqtSignal()       
-    
-    
+    finished = pyqtSignal()
+
+
 class BookViewFunktions():
-    
+
     def loadBooks(self, data):
         self.threadpool = QThreadPool().globalInstance()
         for count, book in enumerate(data):
             book['id'] = count
             worker = DownloadImg(book)
             worker.signals.returnBook.connect(self.bookRecived)
-            worker.signals.finished.connect(lambda: self.workerfinished(len(data)))
+            worker.signals.finished.connect(
+                lambda: self.workerfinished(len(data)))
             self.threadpool.tryStart(worker)
-    
+
     def bookRecived(self, book):
         self.addBook(book)
-    
+
     def workerfinished(self, len):
         if self.containerHoriLayout.count() >= len:
             self.containerHoriLayout.addStretch()
-            
+
     def addBook(self, bookDic):
-        
+
         self.book = QWidget()
         self.title = QLabel()
         self.title.setObjectName('bookTitleLable')
-        
-        if len(bookDic['title']) <= 20: 
+
+        if len(bookDic['title']) <= 20:
             self.title.setText(bookDic['title'])
         else:
             self.title.setText(bookDic['title'][:18] + '...')
-        self.lable = QLabel()
+
+        image = QImage(bookDic['localpath'])
+        self.lable = PictureLabel(image)
         self.lable.setObjectName('bookCoveLable')
-        cover = QPixmap(bookDic['localpath'])
-        cover = cover.scaledToHeight(200)
-        self.lable.setPixmap(cover)
-        
+
         bookLayout = QVBoxLayout()
         bookLayout.addStretch()
         bookLayout.addWidget(self.lable)
         bookLayout.addWidget(self.title)
         bookLayout.addStretch()
         self.book.setLayout(bookLayout)
-        
+
         self.containerHoriLayout.addWidget(self.book)
+
+    def openStatPage(self):
+        self.window = QMainWindow()
+        self.stats = DetailWindow()
+        self.stats.__init__(self.window)
+        self.window.show()
+
+
+class PictureLabel(QLabel):
+
+    pictureClicked = pyqtSignal(object)
+
+    def __init__(self, image, parent=None):
+        super(PictureLabel, self).__init__(parent)
+        cover = QPixmap(image)
+        cover = cover.scaledToHeight(200)
+        self.setPixmap(cover)
+
+    def mousePressEvent(self, event):
+        self.pictureClicked.connect(BookViewFunktions.openStatPage(self))
