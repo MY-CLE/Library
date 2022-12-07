b15648e8024acffa95da87b632e0a57222831f9b
emir1312 <tealaemir@gmail.com>
2022-12-07 22:12:03 +0100

    Update pageselect.py

--- src/gui/pageselect.py
+++ src/gui/pageselect.py
@@ -1,31 +1,36 @@
-from gui.windows.loginWindow import LoginWindow
-from gui.windows.landingWindow import LandingWindow
-from PyQt6.QtWidgets import (QMainWindow,QStackedWidget)
-class PageSelect(QMainWindow):    
+from GUI.Windows.loginWindow import LoginWindow
+from GUI.Windows.landingWindow import LandingWindow
+from GUI.Windows.detailsWindow import DetailWindow
+from PyQt6.QtWidgets import (QMainWindow, QStackedWidget)
+
+
+class PageSelect(QMainWindow):
     def __init__(self):
         super(QMainWindow, self).__init__()
         self.userId = None
-        #initiation of Widows
+        # initiation of Widows
         self.setWindowTitle('Liberary')
         self.loginWindow = LoginWindow(self)
         self.landingWindow = LandingWindow(self)
-        
-        #Add them to ustom Stacked Widget
+        self.detailWindow = DetailWindow(self)
+
+        # Add them to ustom Stacked Widget
         self.pages = QStackedWidget()
         self.pages.addWidget(self.loginWindow)
         self.pages.addWidget(self.landingWindow)
-        #Display the Loginpage as default
-        self.pages.setCurrentIndex(0)
+        self.pages.addWidget(self.detailWindow)
+        # Display the Loginpage as default
+        self.pages.setCurrentIndex(2)
         self.pages.show()
-        
-        #Signals from Children
-        self.loginWindow.sendUser.connect(self.setUser)
-    
+
+        # Signals from Children
+        # self.loginWindow.sendUser.connect(self.setUser)
+
     def setUser(self, user):
         self.userId = user
         print('main window got signal')
         self.changeStackedWidget(1)
         self.landingWindow.setUserid(self.userId)
-        
+
     def changeStackedWidget(self, index):
         self.pages.setCurrentIndex(index)
