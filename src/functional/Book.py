516713bee5ddf9a65f05c054469fa949ce112fd8
mabuit03 <100218844+Doderich@users.noreply.github.com>
2022-12-12 16:15:30 +0100

    restore book

--- src/functional/Book.py
+++ src/functional/Book.py
@@ -1,7 +1,4 @@
-import sys
-sys.path.insert(0, "src//")
-from Rating import rating
-import database.dbconnect as db
+from rating import rating
 
 
 class Book(object):
@@ -14,4 +11,65 @@ class Book(object):
         self.__publisher = publisher
         self.__ratings: rating = []
         self.__isBorrowed: bool = False
+
+    def getTitle(self) -> str:
+        return self.__title
+
+    def setTitle(self, title: str) -> None:
+        self.__title = title
+
+    def getAuthor(self) -> str:
+        return self.__author
+
+    def setAuthor(self, author: str) -> None:
+        self.__author = author
+
+    def getPublishingYear(self) -> int:
+        return self.__publishingYear
+
+    def setPublishingYear(self, publishingYear: int) -> None:
+        self.__publishingYear = publishingYear
+
+    def getEdition(self) -> str:
+        return self.__edition
+
+    def setEdition(self, edition: str) -> None:
+        self.__edition = edition
+
+    def getPublisher(self) -> str:
+        return self.__publisher
+
+    def setPublisher(self, publisher: str) -> None:
+        self.__publisher = publisher
+
+    def getRatings(self) -> list:
+        return self.__ratings
+
+    def addRating(self, newRating: rating) -> None:
+        self.__ratings.append(newRating)
+
+    # get average rating of a book rounded to an int.
+    def getAverageRating(self) -> int:
+        sum = 0
+        for rating in self.__ratings:
+            sum += rating.getValue()
+        return round(sum / len(self.__ratings))
+
+    def isBorrowed(self) -> bool:
+        return self.__isBorrowed
+    
+    def setBorrowed(self) -> None:
+        self.__isBorrowed = True
+    
+    def setUnBorrowed(self) -> None:
+        self.__isBorrowed = False
+
+
+
+book = Book("egal", "egal", 2001, "gal", "egal")
+for i in range(0, 6):
+    book.addRating(rating(i))
+    print(book.getAverageRating())
+    
+
 
