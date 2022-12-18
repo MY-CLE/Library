6cc0ef1ae4b3c395cae53976282a981ede8de4d8
Luurit00 <luurit00@hs-esslingen.de>
2022-12-18 21:04:20 +0100

    Images can now be loaded from db, objects passed to signals instead of dicts

--- src/GUI/elements/guibook.py
+++ src/GUI/elements/guibook.py
@@ -1,27 +1,31 @@
 import os
+import sys
+sys.path.insert(0, "src//")
 from PyQt6.QtWidgets import (QVBoxLayout, QWidget, QLabel)
 from PyQt6.QtGui import QPixmap, QImage
 from PyQt6.QtCore import pyqtSignal
-
+from functional.book import Book
 from gui.windows.detailsWindow import DetailWindow
+
 class GuiBook(QWidget):
     sendClicked = pyqtSignal(int)
-    def __init__(self, bookinfo):
+    def __init__(self, book: Book):
         super(QWidget, self).__init__()
-        self.id = bookinfo['bookid']; 
+        self.id = book.getID()
         title = QLabel()
         title.setObjectName('bookTitleLable')
 
-        if len(bookinfo['title']) <= 20:
-            title.setText(bookinfo['title'])
+        if len(book.getTitle()) <= 20:
+            title.setText(book.getTitle())
         else:
-            title.setText(bookinfo['title'][:18] + '...')
+            title.setText(book.getTitle()[:18]+ '...')
 
         
-        if bookinfo['picture'] == None:
-            bookinfo['picture'] = os.path.join(os.path.abspath('src/assets/books/'), f"50_shades_of_grey.jpg")
-        image = QImage(bookinfo['picture'])
-        lable = PictureLabel(image,title, bookinfo['bookid'])
+        if book.getPicture() == None:
+            book.setPicture(os.path.join(os.path.abspath('src/assets/books/'), f"50ShadesOfGrey.jpg"))
+        #print("TSTSSTTEEFTS" + book.getPicture())
+        image = QImage(os.path.join(os.path.abspath('src/assets/books/'), f"{book.getPicture()}"))
+        lable = PictureLabel(image,title, book.getID())
         lable.clicked.connect(self.bookClicked)
         lable.setObjectName('bookCoveLable')
 
